341946a6bd9d7ba161bbfa90fc8b2d64a851ae89
RobertZH-LA <rzhang29@lacademy.edu>
2025-10-23 16:01:29 -0400

    created subsystems

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveSubsystem.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+public class DriveSubsystem {
+
+    RobotHardware hardware;
+    VisionSubsystem vision;
+
+    public DriveSubsystem(RobotHardware hardware, VisionSubsystem vision) {
+        this.hardware = hardware;
+        this.vision = vision;
+    }
+}
+
+
+
+
+
+
+
+
+
