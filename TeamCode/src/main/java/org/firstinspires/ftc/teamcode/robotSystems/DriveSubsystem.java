93b0e500cfaa6aabab56e3d4626ffaeced876e3f
RobertZH-LA <rzhang29@lacademy.edu>
2025-10-25 08:39:38 -0400

    added drive class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSystems/DriveSubsystem.java
@@ -21,7 +21,33 @@ public class DriveSubsystem {
         hardware.getRightBack().setPower(0);
         hardware.getLeftFront().setPower(0);
         hardware.getRightFront().setPower(0);
+
     }
+    public void drive(double forward, double strafe, double rotateLeft, double rotateRight, boolean stickPressed) {
+        double y = forward;
+        double x = strafe;
+        double rotate = rotateRight - rotateLeft;
+        //Slows speed of wheels
+        double dampening;
+        if(stickPressed) {
+            dampening = .8;
+        }else{
+            dampening = .6;
+        }
+
+        //Calculating the power for the wheels
+        double frontLeftPower = (y + x + rotate) * dampening;
+        double backLeftPower = (y - x + rotate) * dampening;
+        double frontRightPower = (y - x - rotate) * dampening;
+        double backRightPower = (y + x - rotate) * dampening;
+
+        //Set Power
+        leftFront.setPower(frontLeftPower);
+        rightFront.setPower(frontRightPower);
+        leftBack.setPower(backLeftPower);
+        rightBack.setPower(backRightPower);
+    }
+
 }
 
 
