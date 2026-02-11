3af47c8f2b22b7b297e02709298b16c832b1ebed
fortniteforfun <nikhiltiwarima@gmail.com>
2026-02-11 14:57:53 -0500

    Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinwheelSlot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinwheelSlot.java
@@ -4,8 +4,21 @@ public class PinwheelSlot {
     private double position;
     private String color;
 
-    public PinwheelSlot(double position) {
-        this.position = position;
+    private final double pinwheelIncrement=0.4/3;
+
+    public PinwheelSlot(int position) {
+        if (position== 0){
+            this.position=0;
+        } else if (position==1) {
+            this.position = pinwheelIncrement;
+        } else {
+            this.position=pinwheelIncrement*2;
+        }
         this.color = "No Ball";
+    }
+
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSystems/PinwheelSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSystems/PinwheelSubsystem.java
@@ -1,15 +1,45 @@
 package org.firstinspires.ftc.teamcode.robotSystems;
 
+import org.firstinspires.ftc.teamcode.PinwheelSlot;
+
 public class PinwheelSubsystem {
     RobotHardware hardware;
+    PinwheelSlot[] heldArtifacts = {new PinwheelSlot(0),
+                                    new PinwheelSlot(1),
+                                    new PinwheelSlot(2)};
+    
+    private final int noBall = 0;
+    private final int green = 2;
+    private final int purple = 1;
 
+    int currentIntakePos = 0;
 
     public PinwheelSubsystem(RobotHardware hardware) {
         this.hardware = hardware;
-        
+
+    }
+
+    public void shootBall(){
+
+    }
+
+    public void shootBall(String color){
+
+    }
+
+    public void checkForBall(){
+        if(getHue() !=  noBall){
+            heldArtifacts[currentIntakePos].setColor();
+            currentIntakePos++;
+            if(currentIntakePos == 4){
+                currentIntakePos = 0;
+            }
+        }
+    }
+
+    public int getHue(){
+        return hardware.getPinwheelSensor().argb();
     }
 
 
-
-
-}
+    }
