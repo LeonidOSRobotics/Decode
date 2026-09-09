9be33e1d9bed1032261cd1acdcc0be1e88b97a44
25079LeonidOS <LARobotics@lacademy.edu>
2026-09-09 13:57:56 -0400

    Update TestPedroIntergration.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathingSetUp/PedroAutos/TestPedroIntergration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathingSetUp/PedroAutos/TestPedroIntergration.java
@@ -14,25 +14,25 @@ public class TestPedroIntergration extends PedroBase {
     private PathChain driveStartPosShootPos, driveShootPosEndPos;
 
     private final Pose startPose = new Pose(24.700, 130.054, Math.toRadians(140));//DONT CHANGE
-    private final Pose shootPose = new Pose(51.585, 92.247, Math.toRadians(140));
+ //   private final Pose shootPose = new Pose(51.585, 92.247, Math.toRadians(140));
     private final Pose endPose = new Pose(29.909, 72.084, Math.toRadians(150));
 
     public void buildPaths() {
         driveStartPosShootPos = follower.pathBuilder()
-                .addPath(new BezierLine(startPose, shootPose))
-                .setLinearHeadingInterpolation(startPose.getHeading(), shootPose.getHeading())
+                .addPath(new BezierLine(startPose, endPose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), endPose.getHeading())
                 .build();
-        driveShootPosEndPos = follower.pathBuilder()
+       /* driveShootPosEndPos = follower.pathBuilder()
                 .addPath(new BezierLine(shootPose, endPose))
-                .setLinearHeadingInterpolation(shootPose.getHeading(),endPose.getHeading())
+                .setLinearHeadingInterpolation(endPose.getHeading(),endPose.getHeading())
                 .build();
 
-
+*/
     }
 
 
     public void statePathUpdate() {
        follower.followPath(driveStartPosShootPos);
-        follower.followPath(driveShootPosEndPos);
+     //   follower.followPath(driveShootPosEndPos);
     }
 }
